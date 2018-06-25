f489f4e85068405c62156d7cd8e3f0df2fe43b03
luter <luter@139.com>
2018-06-25 19:10:35 +0800

    persistentUnit自定义

--- src/main/java/com/luter/extjs/ExtjsApplication.java
+++ src/main/java/com/luter/extjs/ExtjsApplication.java
@@ -2,11 +2,16 @@ package com.luter.extjs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 
 @SpringBootApplication
 public class ExtjsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExtjsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ExtjsApplication.class, args);
+    }
+
+
 }
---
+++ src/main/java/com/luter/extjs/base/config/DataSourceConfig.java
@@ -0,0 +1,53 @@
+package com.luter.extjs.base.config;
+
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Slf4j
+@Configuration
+@EnableTransactionManagement
+public class DataSourceConfig {
+
+
+    @Bean
+    @Primary
+    public DataSource dataSource() {
+        log.info("主数据源初始化....");
+        return DruidDataSourceBuilder.create().build();
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+        jpaVendorAdapter.setGenerateDdl(true);
+        jpaVendorAdapter.setShowSql(true);
+        return jpaVendorAdapter;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean lef = new LocalContainerEntityManagerFactoryBean();
+        lef.setPackagesToScan("com.luter.extjs");
+        lef.setDataSource(dataSource());
+        lef.setPersistenceUnitName("DanYe");
+        lef.setJpaVendorAdapter(jpaVendorAdapter());
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.show_sql", "true");
+        properties.setProperty("hibernate.jdbc.fetch_size", "100");
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+
+        lef.setJpaProperties(properties);
+        return lef;
+    }
+
+}
